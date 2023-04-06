40580e41ff73f0fe56f5958b9fc29ce0faf762f8
RITUSHI-HALBA <ritushi443@gmail.com>
2023-04-06 16:14:22 +0530

    project_completed

---
+++ js/index.js
@@ -0,0 +1,36 @@
+// AOS JS
+
+AOS.init({
+    offset: 200,
+    duration: 1000
+});
+
+// menu-btn
+const menuBtn = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn_burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
+
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        hamburger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
+
+        showMenu = true;
+    }
+
+    else {
+        hamburger.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+
+        showMenu = false;
+    }
+}
+
